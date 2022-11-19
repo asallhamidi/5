70c81ea8a46568b2f169d88d032d7e846c2c34d2
Partiran <Partiran@DESKTOP-IR2LBIU>
2022-11-19 10:46:54 -0800

    Add project files.

---
+++ 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("mojodi:");
+            string a = Console.ReadLine();
+            int x = Convert.ToInt32(a);
+            Console.Write("darsad bahre:");
+            string b = Console.ReadLine();
+            double y = Convert.ToDouble(b);
+            Console.Write("mojodi jadid:");
+            string c = Console.ReadLine();
+            int z = Convert.ToInt32(c);
+            Console.Write("entehaye sal:");
+            Console.WriteLine(javab(x, y, z));
+        }//end main
+        private static int javab(int x,double y,int z)
+        {
+            double a = 0;
+            int b = 0;
+            for(int i = 1; x < z; i++)
+            {
+                a = x * (y / 100);
+                x = (int)(x + a);
+                b = i;
+            }
+            return b;
+        }
+    }
+}
